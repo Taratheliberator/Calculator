69a562e7b4e2e0b0fb672343a552ba3facbe0645
Viner Gindullin <Taratheliberator@gmail.com>
2022-09-29 15:58:34 +0500

    Добавлен тернарный оператор в функцию деления

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        try {
-            int c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        } catch (ArithmeticException ae) {        // обрабатываем ошибку деления на ноль
-            System.out.println("Деление на ноль");  // выводим текст ошибки
-        }
+
+        int c = calc.devide.apply(a, b);
+
+        calc.println.accept(c);
+
+
     }
 }
